05978d3156ca519b8f4cdd4e3b48f476ba076cdc
arcadesArena <shreyanshu.dps@gmail.com>
2022-03-26 08:57:30 +0530

    shrey edit

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC5mbDpYSlIvvUo4JJn8AKn_nPuNH4QXgw",
+    authDomain: "hale-tracer-315514.firebaseapp.com",
+    projectId: "hale-tracer-315514",
+    storageBucket: "hale-tracer-315514.appspot.com",
+    messagingSenderId: "456402265928",
+    appId: "1:456402265928:web:9f5ea084fd3e7ea2a6b57d",
+    measurementId: "G-SCPZECS5DJ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
