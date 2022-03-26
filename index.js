7ab1c37a551ab78c7e754e52bf154c76482a4738
arcadesArena <shreyanshu.dps@gmail.com>
2022-03-26 12:08:21 +0530

    shrey

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ document.getElementById("loginForm").addEventListener("submit",(event)=>{
 })
 
 firebase.auth().onAuthStateChanged((user)=>{
-<<<<<<< HEAD
     var teach= ['CEAcPVJHzsROJs6PkcAFCP9YBfs2','NriIkZ3JiRcMXkrZ64BDq3BJ1uv2','CAUS4elywCPsNYtSbcg3QUTNVtf1','4KdjFFhx55O6iRAayXTscBoFSLk1','AYKNvKG3GXU4anaO2vfVmOeh9SL2'];
     if(teach.indexOf(user.uid)!== -1){
         location.replace("pages/teacher/teacher.html")
@@ -11,14 +10,6 @@ firebase.auth().onAuthStateChanged((user)=>{
     else{
         // location.replace("pages/teacher/teacher.html")
         location.replace("pages/invalid.html")
-=======
-    if(user.uid == 't0tGyatVM4g9S1zYtTBNUT04Asp2'){
-        //alert(user.uid)
-        location.replace("pages/teacher/teacher.html")
-    }
-    else if (user.uid == 'AYKNvKG3GXU4anaO2vfVmOeh9SL2') {
-        location.replace("pages/teacher/take_attendance.html")
->>>>>>> c29bd0a88432781275eeb47d0810c70e55ebdfc4
     }
 })
 
