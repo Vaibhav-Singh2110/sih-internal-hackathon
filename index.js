3209b17f909010dbc348c4ed998a3d8dcc12a7c0
Vaibhav-Singh2110 <vaibhavmsd39@gmail.com>
2022-03-26 09:45:01 +0530

    vaibh edit

--- index.js
+++ index.js
@@ -36,3 +36,15 @@ function forgotPass(){
         document.getElementById("error").innerHTML = error.message
     });
 }
+const typed = select('.typed')
+if (typed) {
+  let typed_strings = typed.getAttribute('data-typed-items')
+  typed_strings = typed_strings.split(',')
+  new Typed('.typed', {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000
+  });
+}
